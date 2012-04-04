17a4556da475e25f9007a1251a009e20c0b68f0c
David Magee <davidmagee@David-Magees-MacBook-Pro.local>
2012-04-04 00:31:06 -0400

    Added array function.

--- Magee_David_Assignment2.js
+++ Magee_David_Assignment2.js
@@ -1,44 +1,47 @@
-/* var payGold = true,	
-	gotFree = false,
-	readTablet = true
-;
-    
+//list variables up here;
 
 
+// Boolean Function; 
 var goodRead = function (payGold, gotFree, readTablet) {
+	var didYouRead;
 	if ((payGold || gotFree) && readTablet) {
-		return true;
-	} else {
-		return false;
+		didYouRead = true;
 	}
-}; 
-
-var outcome = goodRead(true, false, true);
-console.log ("Hi, " + goodRead);
-
-		
-
-		
-		
-		// console.log( "You read the tablet, which ultimately tells you " + 
-		// "to board the next shuttle off the planet." );
-	 // } else {
-		// console.log( "You feel restless, and convince yourself that " + 
-		// "boarding a shuttle to take you off planet will help." );
-	//};
-	
-*/
-
-var goodRead = function (payGold, gotFree, readTablet) {
-	if ((payGold || gotFree) && readTablet) {
-		console.log ("Yay!");
-	} else {
-		console.log ("Boo.");
-	return goodRead
+	else {
+		didYouRead = false;
 	}
+	return didYouRead;
 };
 
-goodRead(true, false, true);
+var readConfirm = goodRead(true, false, true);
 
-console.log (goodRead);
+if (readConfirm = true){ 
+	console.log ("You read the tablet, which ultimately tells you " + 
+	"to board the next shuttle off the planet." );
+} else { 
+	console.log ("You feel restless, and convince yourself that " +
+	"boarding a shuttle to take you off planet will help." );
+}; 
+// End Boolean Function; 
+
+// Array Function;
+var peopleInLine = [
+	"Steve",
+	"Captain Renauld",
+	"Susan",
+	"Striker"
+];
+
+var waitingForShuttle = function (peopleInLine) {
+	 	var waiting; 
+	 	for (var i = 0, p = peopleInLine.length; i < p; i++) {
+		console.log ( peopleInLine[i] + " has entered the shuttle." );
+};      console.log ("It's your turn to enter the shuttle.");
+		return waiting;
+};
+
+var leftInLine = waitingForShuttle(peopleInLine)
+// End Array Function;
+
+
 
