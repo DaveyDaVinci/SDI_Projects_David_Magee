44d71c2296564b0dee210278a64508ca16d542ec
David Magee <davidmagee@David-Magees-MacBook-Pro.local>
2012-04-03 22:14:37 -0400

    Updated file.

--- Magee_David_Assignment2.js
+++ Magee_David_Assignment2.js
@@ -1,13 +1,44 @@
-var payGold = true,	
+/* var payGold = true,	
 	gotFree = false,
 	readTablet = true
 ;
+    
 
-var goodRead = Function ( ) {
-	if (( payGold || gotFree ) && readTablet ) { 
-		console.log( "You read the tablet, which ultimately tells you " + 
-		"to board the next shuttle off the planet." );
-	} else { 
-		console.log( "You feel restless, and convince yourself that " + 
-		"boarding a shuttle to take you off planet will help." );
+
+var goodRead = function (payGold, gotFree, readTablet) {
+	if ((payGold || gotFree) && readTablet) {
+		return true;
+	} else {
+		return false;
 	}
+}; 
+
+var outcome = goodRead(true, false, true);
+console.log ("Hi, " + goodRead);
+
+		
+
+		
+		
+		// console.log( "You read the tablet, which ultimately tells you " + 
+		// "to board the next shuttle off the planet." );
+	 // } else {
+		// console.log( "You feel restless, and convince yourself that " + 
+		// "boarding a shuttle to take you off planet will help." );
+	//};
+	
+*/
+
+var goodRead = function (payGold, gotFree, readTablet) {
+	if ((payGold || gotFree) && readTablet) {
+		console.log ("Yay!");
+	} else {
+		console.log ("Boo.");
+	return goodRead
+	}
+};
+
+goodRead(true, false, true);
+
+console.log (goodRead);
+
