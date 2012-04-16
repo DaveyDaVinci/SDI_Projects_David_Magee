9b8b312bef3ccc13f1b8d1c756665faa0b48d3fb
David Magee <davidmagee@David-Magees-MacBook-Pro.local>
2012-04-15 20:19:26 -0400

    Added first object with syntax error for review.

--- Magee_David_Assignment3.js
+++ Magee_David_Assignment3.js
@@ -1,1 +1,19 @@
-alert("JavaScript works!");
+var mainCharacter = { 
+	name: "Soren",
+	certifications: [
+		"R3 Pilot",
+		"PPS Programming and documentation"
+		],
+	age: 23,
+	race: "Human",
+	cash: function (inPocket,
+		prePurch) {
+			20 - 5;
+			return mainCharacter.cash
+	}
+	
+};
+
+var yourCash = mainCharacter.cash(20,5);
+
+console.log( yourCash )
