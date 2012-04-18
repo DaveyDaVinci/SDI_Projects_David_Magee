cbfe2b358fd7d46b7795ba21030e4dbf96067fa2
David Magee <davidmagee@David-Magees-MacBook-Pro.local>
2012-04-18 01:12:54 -0400

    Updated the main file with the second object, and added JSON data.

--- Magee_David_Assignment3.js
+++ Magee_David_Assignment3.js
@@ -1,4 +1,4 @@
-var mainCharacter = { 
+var mainCharacter = { // object
 	name: "Soren",
 	sex: "Male",
 	certifications: [
@@ -7,11 +7,11 @@ var mainCharacter = {
 		],
 	age: 23,
 	race: "Human",
-	cash: function (inPocket,
+	cash: function (inPocket, // method: function
 		prePurch) {
-			return (inPocket - prePurch);
+			return (inPocket - prePurch); // number return
 		}, 
-	catchPhrase: function (){
+	catchPhrase: function (){ // method: procedure
 		if (mainCharacter.sex === "Male"){
 			console.log ("I'm the man of the Earth hour.");
 		
@@ -20,7 +20,7 @@ var mainCharacter = {
 			} return 
 		},
 	addCertification: function (training){ 
-		return mainCharacter.certifications.push(training);
+		return mainCharacter.certifications.push(training); // array return
 	}
 	
 	
@@ -33,6 +33,40 @@ var yourCash = mainCharacter.cash(20,5);
 
 var certAdd = mainCharacter.addCertification("Squad Leadership Training");
 
+
+var mainShip = { // object
+	ownShip: true, 
+	baseModel: "Viper R3",
+	addOns: {
+		boosters: true,
+		stealthField: true,
+		aI: true, 
+		vI: false,
+	}, 
+	crewMembers: [ 
+		"Steven",
+		"Lenne",
+		"Carta",
+		"AnDuk"
+	],
+	addCrew: function (crewMembers) { // method: function
+		var addMember;
+			for (var i = 4, p = crewMembers.length; i < 5; i++) { // for loop
+				mainShip.crewMembers.push("Arthur");
+			}
+			return addMember; // return array
+	},
+	
+	
+};
+
+mainShip.addCrew(mainShip.crewMembers);
+
+
+
+
 // test for returned value: console.log( yourCash );
 
 // test for returned value: console.log (mainCharacter.certifications);
+
+// test for returned value: console.log(mainShip.crewMembers);
---
+++ Magee_David_Assignment3_JSON.js
@@ -0,0 +1,56 @@
+var json = {
+	"crewList": {
+		"member1": {
+			"name": "Steven",
+			"rank": "Hand", 
+			"age": 32,
+			"certifications" : [ "Basic training", "CCaR" ],
+			"homePlanet": "Earth",
+			"loyal": true
+			},
+		"member2": {
+			"name": "Lenne,
+			"rank": "Official", 
+			"age": 36,
+			"certifications" : [ "Basic training", "Classified Espionage Trainings" ],
+			"homePlanet": "Mars",
+			"loyal": false
+			},
+		"member3": {
+			"name": "Carta",
+			"rank": "Doctor", 
+			"age": 407,
+			"certifications" : [ "Med Graduate", "CCaR" ],
+			"homePlanet": "Aysis",
+			"loyal": true
+			},
+		"member4": {
+			"name": "AnDuk",
+			"rank": "Official", 
+			"age": 66,
+			"certifications" : [ "Gorian War Training", "Juror of Nomius" ],
+			"homePlanet": "Nomius",
+			"loyal": true
+			},
+			"member5": {
+			"name": "Arthur",
+			"rank": "Hand", 
+			"age": 22,
+			"certifications" : [ "Basic training", "Graduate of Astrophysics at UoM" ],
+			"homePlanet": "Earth",
+			"loyal": false
+			},
+	}, 
+	
+			
+};
+
+
+// have the main js file reference a particular crew member to read his/her profile.
+
+
+/*"newCert": true, 
+			"addCert": function (){
+				if ( json.crewList.member1.newCert === true) {
+					return json.crewList.member1.certifications.push();
+*/
